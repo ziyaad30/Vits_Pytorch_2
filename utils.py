e2325d5d18b685a9948d05a2f403378e7972684a
Ziyaad <xavierbaatjes@yahoo.com>
2023-09-19 20:51:02 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -172,7 +172,7 @@ def load_filepaths_and_text(filename, split="|"):
 
 def get_hparams(init=True):
     parser = argparse.ArgumentParser()
-    parser.add_argument( "-c", "--config", type=str, default="./configs/vits2_ljs_nosdp.json", help="JSON file for configuration")
+    parser.add_argument( "-c", "--config", type=str, default="./configs/vits2_ljs_nosdp_drive.json", help="JSON file for configuration")
     parser.add_argument("-m", "--model", type=str, default="/content/drive/MyDrive/vits_pytorch_2/ljs_base", help="Model name")
 
     args = parser.parse_args()
