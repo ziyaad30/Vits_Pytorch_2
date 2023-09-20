ce187b1e7023019cfb4ff72cfb36fe1e64412e3c
Ziyaad <xavierbaatjes@yahoo.com>
2023-09-20 07:57:30 +0200

    Update train_ms.py

--- train_ms.py
+++ train_ms.py
@@ -96,7 +96,7 @@ def run(rank, n_gpus, hps):
     collate_fn = TextAudioSpeakerCollate()
     train_loader = DataLoader(
         train_dataset,
-        num_workers=8,
+        num_workers=4,
         shuffle=False,
         pin_memory=True,
         collate_fn=collate_fn,
@@ -106,7 +106,7 @@ def run(rank, n_gpus, hps):
         eval_dataset = TextAudioSpeakerLoader(hps.data.validation_files, hps.data)
         eval_loader = DataLoader(
             eval_dataset,
-            num_workers=8,
+            num_workers=4,
             shuffle=False,
             batch_size=hps.train.batch_size,
             pin_memory=True,
